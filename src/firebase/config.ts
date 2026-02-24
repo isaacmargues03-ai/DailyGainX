b9d0f1b735ee669c30e679b70841fa2ee421bad5
Isaac margues de jesus <isaacmargues03@gmail.com>
2026-02-24 08:08:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3390225262-e95c8",
+  "appId": "1:464652136616:web:380e9c18f966c4027e529d",
+  "apiKey": "AIzaSyA0FdtKYxBOorb5kQ0oXT4Hd97Oj2fleMY",
+  "authDomain": "studio-3390225262-e95c8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "464652136616"
+};
